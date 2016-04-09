f1039006e430458059fa8d92c1d65a4a33f15639
Jon Harvey <jon@jon-harvey.com>
2016-04-08 20:02:01 -0700

    initial upload

---
+++ WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace WindowsFormsApplication3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnStop = new System.Windows.Forms.Button();
+            this.txtArtist = new System.Windows.Forms.TextBox();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.txtStatus = new System.Windows.Forms.TextBox();
+            this.txtMetadata = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.btnStart.Location = new System.Drawing.Point(170, 386);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(128, 34);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnStop
+            // 
+            this.btnStop.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStop.Enabled = false;
+            this.btnStop.Location = new System.Drawing.Point(340, 386);
+            this.btnStop.Name = "btnStop";
+            this.btnStop.Size = new System.Drawing.Size(147, 34);
+            this.btnStop.TabIndex = 1;
+            this.btnStop.Text = "Stop";
+            this.btnStop.UseVisualStyleBackColor = true;
+            this.btnStop.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txtArtist
+            // 
+            this.txtArtist.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtArtist.Location = new System.Drawing.Point(107, 22);
+            this.txtArtist.Name = "txtArtist";
+            this.txtArtist.Size = new System.Drawing.Size(514, 22);
+            this.txtArtist.TabIndex = 2;
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtTitle.Location = new System.Drawing.Point(107, 69);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(514, 22);
+            this.txtTitle.TabIndex = 3;
+            // 
+            // txtStatus
+            // 
+            this.txtStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtStatus.Location = new System.Drawing.Point(107, 119);
+            this.txtStatus.Name = "txtStatus";
+            this.txtStatus.Size = new System.Drawing.Size(514, 22);
+            this.txtStatus.TabIndex = 4;
+            // 
+            // txtMetadata
+            // 
+            this.txtMetadata.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMetadata.Location = new System.Drawing.Point(107, 167);
+            this.txtMetadata.Multiline = true;
+            this.txtMetadata.Name = "txtMetadata";
+            this.txtMetadata.Size = new System.Drawing.Size(514, 202);
+            this.txtMetadata.TabIndex = 5;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(53, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Artist :";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(53, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(43, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Title :";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(40, 119);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Status :";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(21, 170);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(75, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Metadata :";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.TopRight;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 432);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMetadata);
+            this.Controls.Add(this.txtStatus);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.txtArtist);
+            this.Controls.Add(this.btnStop);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SONOS Skype Status Updater Service Tester App";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnStop;
+        private System.Windows.Forms.TextBox txtArtist;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.TextBox txtStatus;
+        public System.Windows.Forms.TextBox txtMetadata;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Lync.Model;
+using System.Net;
+using System.IO;
+using System.Xml;
+using System.Text.RegularExpressions;
+using System.Timers;
+using System.Collections.Specialized;
+
+namespace WindowsFormsApplication3
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetArtistText(_curArtist);
+            SetTitleText(_curTitle);
+            SetStatusText(_curStatus);
+        }
+
+        private bool _isRunning = false;
+        private String _curStatus = null;
+        private String _curArtist = null;
+        private String _curTitle = null;
+        private String _initStatus = null;
+        private LyncClient _LyncClient = null;
+        private String _sonosHost = null;
+        private NameValueCollection _appSettings;
+        private System.Timers.Timer _connectionTimer = null;
+
+        #region Form Events
+        private void button1_Click(object sender, EventArgs e)
+        {
+            _appSettings = System.Configuration.ConfigurationManager.AppSettings;
+            _sonosHost = _appSettings["sonosIP"];
+            _LyncClient = LyncClient.GetClient();
+            _initStatus = PollStatus();
+            _connectionTimer = new System.Timers.Timer(5000);
+            _connectionTimer.Elapsed += new ElapsedEventHandler(this.TickTock);
+            _connectionTimer.Enabled = true;
+            _connectionTimer.Start();
+            this.UpdateRunningState();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            _connectionTimer.Stop();
+            MessageBox.Show("init: " + _initStatus);
+            UpdateStatus(_initStatus);
+            this.UpdateRunningState();
+        }
+        #endregion
+
+        #region Lync Functions
+        private String PollStatus() 
+        {
+            return _LyncClient.Self.Contact.GetContactInformation(ContactInformationType.LocationName).ToString();
+        }
+        private void UpdateStatus(String status)
+        {
+            Dictionary<PublishableContactInformationType, object> stuff = new Dictionary<PublishableContactInformationType, object>();
+            stuff.Add(PublishableContactInformationType.LocationName, status);
+            _LyncClient.Self.BeginPublishContactInformation(stuff,
+                (ar) =>
+                {
+                    _LyncClient.Self.EndPublishContactInformation(ar);
+                }
+                ,
+            null);
+            this._curStatus = status;
+        }
+        #endregion
+
+        #region SONOS Functions
+        private bool PollSonos()
+        {
+            bool retVal = true;
+            String service = "urn:schemas-upnp-org:service:AVTransport:1";
+            String action = "GetPositionInfo";
+
+            // Build the headers
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create("http://" + _sonosHost + "/MediaRenderer/AVTransport/Control");
+            try
+            {
+                req.Headers.Add("SOAPACTION", service + "#" + action);
+                req.ContentType = "text/xml;charset=\"utf-8\"";
+                req.Method = "POST";
+                //req.Timeout = System.Threading.Timeout.Infinite;
+
+                // Write the Request XML
+                StreamWriter writer = new StreamWriter(req.GetRequestStream(), new UTF8Encoding(false));
+                writer.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\"?><s:Envelope s:encodingStyle=\"http://schemas.xmlsoap.org/soap/encoding/\" xmlns:s=\"http://schemas.xmlsoap.org/soap/envelope/\"><s:Body><u:GetPositionInfo xmlns:u=\"urn:schemas-upnp-org:service:AVTransport:1\"><InstanceID>0</InstanceID></u:GetPositionInfo></s:Body></s:Envelope>");
+                writer.Close();
+
+                // Get the Response XML
+                WebResponse resp = req.GetResponse();
+                XmlReader r = new XmlTextReader(resp.GetResponseStream());
+                bool nextNode = false;
+                String metadata = null;
+                while (r.Read())
+                {
+                    switch (r.NodeType)
+                    {
+                        case XmlNodeType.Element:
+                            if (r.Name == "TrackMetaData")
+                                nextNode = true;
+                            break;
+                        case XmlNodeType.Text:
+                            if (nextNode)
+                            {
+                                metadata = r.Value;
+                                nextNode = false;
+                            }
+                            break;
+                    }
+                }
+                Regex regex = new Regex(".*<dc:creator>(.*)</dc:creator>.*");
+                Match match = regex.Match(metadata);
+                if (match.Success)
+                    _curArtist = System.Web.HttpUtility.HtmlDecode(match.Groups[1].Value);
+                else
+                {
+                    _curArtist = "";
+                    retVal = false;
+                }
+                regex = new Regex(".*<dc:title>(.*)</dc:title>.*");
+                match = regex.Match(metadata);
+                if (match.Success)
+                    _curTitle = System.Web.HttpUtility.HtmlDecode(match.Groups[1].Value);
+                else
+                {
+                    _curTitle = "";
+                    retVal = false;
+                }
+                SetMetadataText(metadata);
+            }
+            catch (Exception e)
+            {
+                SetMetadataText("[" + System.DateTime.Now.ToShortTimeString() + "] " + e.Message);
+            }
+            return retVal;
+        }
+        private void PollSonosFilepath() 
+        {
+            String metadata = txtMetadata.Text;
+            Regex regex = new Regex(".*<res.*>(.*)</res>.*");
+            Match match = regex.Match(metadata);
+            String filepath = System.Web.HttpUtility.HtmlDecode(match.Groups[1].Value);
+            regex = new Regex(".*MUSIC.*/(.*).mp3");
+            match = regex.Match(filepath);
+            if (match.Success)
+            {
+                String[] temp = System.Web.HttpUtility.HtmlDecode(match.Groups[1].Value).Split('-');
+//                MessageBox.Show(String.Format("{0}::{1}", temp[0], temp[1]));
+                _curArtist = temp[0].Trim();
+                _curTitle = temp[1].Trim();
+            }
+            return;
+        }
+        #endregion
+
+        #region Utility Functions
+        delegate void SetMetadataTextCallback(string text);
+        private void SetMetadataText(String newtext)
+        {
+            if (this.txtMetadata.InvokeRequired)
+            {
+                SetMetadataTextCallback d = new SetMetadataTextCallback(SetMetadataText);
+                this.Invoke(d, new object[] { newtext });
+            }
+            else
+            {
+                this.txtMetadata.Text = newtext;
+            }
+        }
+        delegate void SetArtistTextCallback(string text);
+        private void SetArtistText(String newtext)
+        {
+            if (this.txtArtist.InvokeRequired)
+            {
+                SetArtistTextCallback d = new SetArtistTextCallback(SetArtistText);
+                this.Invoke(d, new object[] { newtext });
+            }
+            else
+            {
+                this.txtArtist.Text = newtext;
+            }
+        }
+        delegate void SetTitleTextCallback(string text);
+        private void SetTitleText(String newtext)
+        {
+            if (this.txtTitle.InvokeRequired)
+            {
+                SetTitleTextCallback d = new SetTitleTextCallback(SetTitleText);
+                this.Invoke(d, new object[] { newtext });
+            }
+            else
+            {
+                this.txtTitle.Text = newtext;
+            }
+        }
+        delegate void SetStatusTextCallback(string text);
+        private void SetStatusText(String newtext)
+        {
+            if (this.txtStatus.InvokeRequired)
+            {
+                SetStatusTextCallback d = new SetStatusTextCallback(SetStatusText);
+                this.Invoke(d, new object[] { newtext });
+            }
+            else
+            {
+                this.txtStatus.Text = newtext;
+            }
+        }
+        private void TickTock(object sender, ElapsedEventArgs e)
+        {
+            if (!PollSonos())
+                PollSonosFilepath();
+            if (PollStatus() != _appSettings["message"].Replace("$artist", _curArtist).Replace("$title", _curTitle))
+            {
+                UpdateStatus(_appSettings["message"].Replace("$artist", _curArtist).Replace("$title", _curTitle));
+            }
+            SetArtistText(_curArtist);
+            SetTitleText(_curTitle);
+            SetStatusText(_curStatus);
+        }
+        private void UpdateRunningState() 
+        {
+            btnStart.Enabled = !btnStart.Enabled;
+            btnStop.Enabled = !btnStop.Enabled;
+            _isRunning = !_isRunning;
+            return;
+        }
+        #endregion
+
+    }
+}
